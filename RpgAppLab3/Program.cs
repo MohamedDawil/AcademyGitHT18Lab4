8b87e1bf08e0000055274d05b6f19e2efe8a2ecc
Mohamed <mohamed.a.dawil@outlook.com>
2018-11-01 10:48:27 +0100

    Added a name in elven-names

--- RpgAppLab3/Program.cs
+++ RpgAppLab3/Program.cs
@@ -17,7 +17,7 @@ namespace RpgAppLab3
             }
             {
                 var allNames = new[] { "Briana", "Ben",
-                    "David", "Dana", "Felicia", "Felix", "Legolas" };
+                    "David", "Dana", "Felicia", "Felix", "Legolas", "jhsffjh" };
                 var randomIndex = rnd.Next(allNames.Length);
                 Console.WriteLine(allNames[randomIndex]);
             }
