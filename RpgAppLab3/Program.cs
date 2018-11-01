194ee71eafb1dc43906d4eefc7141fbdd22cfd1f
Mohamed <mohamed.a.dawil@outlook.com>
2018-11-01 10:32:45 +0100

    Added

---
+++ RpgAppLab3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RpgAppLab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            Console.WriteLine("What do you need?");
+            var command = Console.ReadLine();
+            if (command == "roll D20")
+                var diceRoll = rnd.Next(20) + 1;
+            Console.WriteLine($"Your dice roll is: {diceRoll}");
+            {
+                var allNames = new[] { "Briana", "Ben",
+                    "David", "Dana", "Felicia", "Felix" };
+                var randomIndex = rnd.Next(allNames.Length);
+                Console.WriteLine(allNames[randomIndex]);
+            }
+            Console.ReadKey(); }
+    }
+}
